0c71df9366e3f9d10e4822e4ad769b06845abc12
Admir Skomorac <admir.skomorac@outlook.com>
2024-02-28 20:16:52 +0000

    update of variables in functions

--- .learn/resets/00-welcome/app.js
+++ .learn/resets/00-welcome/app.js
@@ -8,28 +8,24 @@ console.log(sum(7,3))
 
 // Export the function to be used on other files 
 // (similar to the keyword "export" when using webpack)
-let oneEuroIs = {
-    "JPY": 156.5, // japan yen
-    "USD": 1.07, // us dollar
-    "GBP": 0.87, // british pound
-}
+
 // We declare the function with the exact name "fromEuroToDollar"
 const fromEuroToDollar = function(valueInEuro) {
     // Convert the given valueInEuro to dollars
-    let valueInDollar = valueInEuro * oneEuroIs["USD"];
+    let valueInDollar = valueInEuro * 1.07;
     // return the dollar value
     return valueInDollar;
 };
 
 const fromDollarToYen = function (valueinDollar) {
-    let valueInEuro = valueinDollar / oneEuroIs["USD"];
-    let valueInYen = valueInEuro * oneEuroIs["JPY"];
+    let valueInEuro = valueinDollar / 1.07;
+    let valueInYen = valueInEuro * 156.5;
     return valueInYen;
 };
 
 const fromYenToPound = function (valueInYen) {
-    let valueInEuro = valueInYen / oneEuroIs["JPY"];
-    let valueInPound = valueInEuro * oneEuroIs["GBP"];
+    let valueInEuro = valueInYen / 156.5;
+    let valueInPound = valueInEuro * 0.87;
     return valueInPound;
 };
 
